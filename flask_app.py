a9a23c958fc9078363e5703ce049c4897c93103f
jnh17331 <jnh17331@gmail.com>
2023-10-08 17:09:13 -0400

    Getting rid of basic frontend

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
 from flask import Flask, jsonify, request, render_template
-#from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, template_folder="../personal_website")
+CORS(app)
 
 @app.route('/')
 def home():
@@ -21,6 +22,11 @@ def projects():
 def about():
     return "Under Construction"
 
+@app.route('/api/test', methods=['GET'])
+def get_data():
+    data = {"message": "Testing API with Flask"}
+    return jsonify(data)
+
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(port=3000, debug=True)
 
