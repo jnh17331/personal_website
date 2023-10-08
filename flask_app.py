bdc3a250b44bcdb9974e7cd9be2642c70ea988a8
jnh17331 <jnh17331@gmail.com>
2023-10-07 23:43:23 -0400

    Added Flask

---
+++ flask_app.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+from flask import Flask, jsonify, request, render_template
+#from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, template_folder="../personal_website")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return "Under Construction"
+
+@app.route('/projects', methods=['GET'])
+def projects():
+    return "This is Also Under Construction"
+
+@app.route('/about', methods=['GET'])
+def about():
+    return "Under Construction"
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
+
