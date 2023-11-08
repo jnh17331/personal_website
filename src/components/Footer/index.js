03d72730bb489723117e870698823485f9781ea2
jnh17331 <jnh17331@gmail.com>
2023-11-07 19:37:20 -0500

    Added sticking to navbar

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import './index.scss'
 import githublogo from '../../assets/images/githublogo.png'
 import instalogo from '../../assets/images/instalogo.png'
@@ -6,27 +7,30 @@ import linkedinlogo from '../../assets/images/linkedinlogo.png'
 import spotifylogo from '../../assets/images/spotifylogo.png'
 
 
-const Footer = () => (
-    <div className='container'>
-        <div className='footer'>
-            <ul>
-                <li>
-                    <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/jesse-heaton-b777511b9/'>
-                        <img src={linkedinlogo} alt="LinkedIn"/>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href='https://github.com/jnh17331'>
-                        <img src={githublogo} alt="Github Logo"/>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href='https://www.instagram.com/_jesse_heaton_/'>
-                        <img src={instalogo} alt="Instagram"/>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href='https://open.spotify.com/user/cornyquotient15?si=dc5898e873f1481c&nd=1'>
-                        <img src={spotifylogo} alt="Spotify"/>
-                    </a>
-                </li>
-            </ul>
+const Footer = () => {
+    return (
+    
+        <div className='container'>
+            <div className='footer'>
+                <ul>
+                    <li>
+                        <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/jesse-heaton-b777511b9/'>
+                            <img src={linkedinlogo} alt="LinkedIn"/>
+                        </a>
+                        <a target="_blank" rel="noreferrer" href='https://github.com/jnh17331'>
+                            <img src={githublogo} alt="Github Logo"/>
+                        </a>
+                        <a target="_blank" rel="noreferrer" href='https://www.instagram.com/_jesse_heaton_/'>
+                            <img src={instalogo} alt="Instagram"/>
+                        </a>
+                        <a target="_blank" rel="noreferrer" href='https://open.spotify.com/user/cornyquotient15?si=dc5898e873f1481c&nd=1'>
+                            <img src={spotifylogo} alt="Spotify"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Footer;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,32 +3,54 @@ import './index.scss'
 import pfp from '../../assets/images/pfp.JPG'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope, faSitemap, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from 'react';
 
-const Navbar = () => (
-    <div className='container'>
-        <div className='nav-bar'>
-            <Link className='profile-pic' to='/'>
-                {/*<img src={pfp} alt='pfp' className='profile-image' />*/}
-            </Link>
-            <nav>
-                <NavLink exact="true" activeclassname='active' className='about-link' to='/about'>
-                    <FontAwesomeIcon icon={faUser} color='1f1f1f' />
-                </NavLink>
-                <NavLink exact="true" activeclassname='active' className='projects' to='/projects'>
-                    <FontAwesomeIcon icon={faSitemap} color='1f1f1f' />
-                </NavLink>
-                <NavLink exact="true" activeclassname='active' className='home' to='/'>
-                    <FontAwesomeIcon icon={faHome} color='1f1f1f' />
-                </NavLink>
-                <NavLink exact="true" activeclassname='active' className='blog' to='/blog'>
-                    <FontAwesomeIcon icon={faGlobe} color='1f1f1f' />
-                </NavLink>
-                <NavLink exact="true" activeclassname='active' className='contact-link' to='/contact'>
-                    <FontAwesomeIcon icon={faEnvelope} color='1f1f1f' />
-                </NavLink>
-            </nav>
-        </div>
-    </div>
-);
+const Navbar = () => {
+
+    const [isSticky, setSticky] = useState(false);
+
+  
+    const handleScroll = () => {
+        if (window.scrollY > 0) {
+        setSticky(true);
+        } else {
+        setSticky(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+            <div className='container'>
+                <div className={`nav-bar ${isSticky ? 'sticky' : ''}`}>
+                    <Link className='profile-pic' to='/'>
+                        {/*<img src={pfp} alt='pfp' className='profile-image' />*/}
+                    </Link>
+                    <nav>
+                        <NavLink exact="true" activeclassname='active' className='about-link' to='/about'>
+                            <FontAwesomeIcon icon={faUser} color='1f1f1f' />
+                        </NavLink>
+                        <NavLink exact="true" activeclassname='active' className='projects' to='/projects'>
+                            <FontAwesomeIcon icon={faSitemap} color='1f1f1f' />
+                        </NavLink>
+                        <NavLink exact="true" activeclassname='active' className='home' to='/'>
+                            <FontAwesomeIcon icon={faHome} color='1f1f1f' />
+                        </NavLink>
+                        <NavLink exact="true" activeclassname='active' className='blog' to='/blog'>
+                            <FontAwesomeIcon icon={faGlobe} color='1f1f1f' />
+                        </NavLink>
+                        <NavLink exact="true" activeclassname='active' className='contact-link' to='/contact'>
+                            <FontAwesomeIcon icon={faEnvelope} color='1f1f1f' />
+                        </NavLink>
+                    </nav>
+                </div>
+            </div>
+    );
+};
 
 export default Navbar;
