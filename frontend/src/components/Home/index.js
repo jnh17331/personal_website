a8b22ec7be4dea73ebaef381c0922abfe2efcf9e
jnh17331 <jnh17331@gmail.com>
2024-01-24 21:06:59 -0500

    Added pictures back to homepage

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,13 +2,13 @@ import './index.scss';
 import React, { useEffect, useState}  from 'react';
 import { Link } from 'react-router-dom';
 import ArrayDisplayer from '../ArrayDisplayer';
-// import pfp from '../../assets/images/pfp.JPG';
-// import ml_music from '../../assets/images/projects/ml_music.png';
-// import pokedex from '../../assets/images/projects/pokedex.png';
-// import movie_analysis from '../../assets/images/projects/movie_analysis.png';
-// import eq_leaflet from '../../assets/images/projects/eq_leaflet.png';
-// import mars_webscraping from '../../assets/images/projects/mars_webscraping.png';
-// import website from '../../assets/images/projects/website_capture.png';
+import pfp from '../../assets/images/pfp.JPG';
+import ml_music from '../../assets/images/projects/ml_music.png';
+import pokedex from '../../assets/images/projects/pokedex.png';
+import movie_analysis from '../../assets/images/projects/movie_analysis.png';
+import eq_leaflet from '../../assets/images/projects/eq_leaflet.png';
+import mars_webscraping from '../../assets/images/projects/mars_webscraping.png';
+import website from '../../assets/images/projects/website_capture.png';
 
 const Home = () => {
     const [projectlist, setProjects] = useState([]);
@@ -55,14 +55,14 @@ const Home = () => {
                 <div className='introduction'>
                     <h1>Hello world,</h1>
                     <p>My name is Jesse and I am a Data Scientist.</p>
-                    {/* <img  className='picture' src={pfp} alt='Me'/> */}
+                    <img  className='picture' src={pfp} alt='Me'/>
                 </div>
                 <div id="mirrored-stars-container">
                     <div id='mirrored-stars'></div>
                     <div id='mirrored-stars2'></div>
                     <div id='mirrored-stars3'></div>
                 </div>
-                {/* <div className='cube-container'>
+                <div className='cube-container'>
                     <Link to='/projects'>
                         <div className='cube'>
                             <div className='face front'>
@@ -85,7 +85,7 @@ const Home = () => {
                             </div>
                         </div>
                     </Link>
-                </div> */}
+                </div>
                 <div className='project-textzone'>
                     <h1><ArrayDisplayer items={itemTitles} /></h1>
                     <ArrayDisplayer items={itemsDescriptions} />
